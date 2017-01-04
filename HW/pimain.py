58a5b06df90448c385a754537299b003b523fa95
pauline <pao-lin@comcast.net>
2017-01-04 13:24:07 -0800

    changed GPHOME to GPHOME

--- HW/pimain.py
+++ HW/pimain.py
@@ -829,10 +829,10 @@ if __name__ == "__main__":
             print "Moving Laser to home switch \r\n"
 
         if (rcv == "GPHOME"):
-            # Move_GelPump_Home = 1
-            # print "Moving Gel Pump to Home switch \r\n"
-            move_gel_pump_up = 1
-            print "Moving Gel Pump UP \r\n"
+            Move_GelPump_Home = 1
+            print "Moving Gel Pump to Home switch \r\n"
+            # move_gel_pump_up = 1
+            # print "Moving Gel Pump UP \r\n"
             # Move_GelPump_Start = 1
             # print "Moving Gel Pump to Start switch \r\n"
 
